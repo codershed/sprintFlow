9dca6166b73f011e27ed5fb9fba593018887d1eb
Francois Mienie <codershed@gmail.com>
2026-03-15 13:42:39 +0200

    added the Assigned Roles on the user profile button

--- Client/codershed.sprintflow/sprintFlow/Factories/CustomUserFactory.cs
+++ Client/codershed.sprintflow/sprintFlow/Factories/CustomUserFactory.cs
@@ -10,19 +10,46 @@ public class CustomUserFactory : AccountClaimsPrincipalFactory<RemoteUserAccount
     public override async ValueTask<ClaimsPrincipal> CreateUserAsync(RemoteUserAccount account, RemoteAuthenticationUserOptions options)
     {
         var user = await base.CreateUserAsync(account, options);
-        var claimsIdentity = (ClaimsIdentity)user.Identity;
 
-        if (account != null && account.AdditionalProperties.TryGetValue("roles", out var roles))
+        if (user.Identity is ClaimsIdentity identity && identity.IsAuthenticated)
         {
-            if (roles is JsonElement element && element.ValueKind == JsonValueKind.Array)
+            // Handle roles claim - it may come as a JsonElement array
+            var rolesClaim = identity.FindFirst("roles");
+            if (rolesClaim is not null && rolesClaim.Value.StartsWith('['))
             {
-                // Unpack the JSON array into individual C# claims
-                foreach (var item in element.EnumerateArray())
+                identity.RemoveClaim(rolesClaim);
+
+                var roles = JsonSerializer.Deserialize<string[]>(rolesClaim.Value);
+                if (roles is not null)
                 {
-                    claimsIdentity.AddClaim(new Claim(options.RoleClaim, item.GetString()));
+                    foreach (var role in roles)
+                    {
+                        identity.AddClaim(new Claim(identity.RoleClaimType, role));
+                    }
                 }
             }
+            else if (rolesClaim is not null)
+            {
+                // Single role as string - add it as role claim type
+                identity.RemoveClaim(rolesClaim);
+                identity.AddClaim(new Claim(identity.RoleClaimType, rolesClaim.Value));
+            }
         }
+
+        //var claimsIdentity = (ClaimsIdentity)user.Identity;
+
+        //if (account != null && account.AdditionalProperties.TryGetValue("roles", out var roles))
+        //{
+        //    if (roles is JsonElement element && element.ValueKind == JsonValueKind.Array)
+        //    {
+        //        Unpack the JSON array into individual C# claims
+        //        foreach (var item in element.EnumerateArray())
+        //        {
+        //            claimsIdentity.AddClaim(new Claim(options.RoleClaim, item.GetString()));
+
+        //        }
+        //    }
+        //}
         return user;
     }
 }
